83f9252760aa9cbf05767e0cec320458c6ece2c3
Meng Gao <mirogm@gmail.com>
2020-05-12 16:30:05 -0400

    considering GNP in water

--- notebook/myfun.py
+++ notebook/myfun.py
@@ -1,3 +1,23 @@
+import numpy as np
+import PyMieScatt as ps
+
+def run_mie(nv, kv, wv, dv):
+    nd=len(dv)
+    nwv=len(wv)
+    
+    #run the mie code for above parameters
+    miev=np.array([[ps.MieQ(nv[i]+kv[i]*1.0j,wv[i],dv[j],asDict=True) \
+                    for j in range(nd)] \
+                   for i in range(nwv)])
+
+    qextv=np.array([[miev[i,j]['Qext'] for j in range(nd)]for i in range(nwv)])
+    qscav=np.array([[miev[i,j]['Qsca'] for j in range(nd)]for i in range(nwv)])
+    qabsv=np.array([[miev[i,j]['Qabs'] for j in range(nd)]for i in range(nwv)])
+    qbackv=np.array([[miev[i,j]['Qback'] for j in range(nd)]for i in range(nwv)])
+    gv=np.array([[miev[i,j]['g'] for j in range(nd)]for i in range(nwv)])
+
+    return [qextv,qscav, qabsv, qbackv,gv]
+
 def plot_q(ax, qextv,  wv, dv, yscale='log',xlabel="Wavelength(nm)", ylabel='', figsize=(4,4)):
     nd=len(dv)
     nwv=len(wv)
